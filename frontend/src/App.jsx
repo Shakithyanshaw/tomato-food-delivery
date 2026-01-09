c56b9828aff753136e66c47c7dda011ff73ed9ee
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-01-10 01:03:09 +0530

    code header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
-        <Routes path="/order" element={<PlaceOrder />} />
+        <Route path="/order" element={<PlaceOrder />} />
       </Routes>
     </div>
   );
