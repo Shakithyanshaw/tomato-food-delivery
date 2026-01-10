e1de24d15eb3267bcea3a319c51a42ecd4a0880a
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-01-10 13:11:56 +0530

    Add FoodItem component with state management for item count; update FoodDisplay to render FoodItem

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import './FoodDisplay.css';
 import { StoreContext } from '../../context/storeContext';
+import FoodItem from '../FoodItem/FoodItem';
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
@@ -9,9 +10,18 @@ const FoodDisplay = ({ category }) => {
     <div>
       <div className="food-display" id="food-display">
         <h2>Top dishes nere you</h2>
-        <div className="food-diisplay-list">
+        <div className="food-display-list">
           {food_list.map((item, index) => {
-            return;
+            return (
+              <FoodItem
+                key={index}
+                id={item.id}
+                name={item.name}
+                price={item.price}
+                description={item.description}
+                image={item.image}
+              />
+            );
           })}
         </div>
       </div>
