8b937033b4624aaad9e788c50e1e19a797bee6f3
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-01-11 13:02:49 +0530

    Add Footer component with content and styles; integrate into App component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
 import './Footer.css';
+import { assets } from '../../assets/assets';
 
 const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Enjoy fresh, delicious meals delivered straight to your doorstep
+            with our food delivery service. We partner with trusted restaurants
+            to bring you a wide variety of cuisines, fast delivery, and reliable
+            service—all at your convenience. Whether you’re craving comfort food
+            or trying something new, we’re here to make every meal easy, tasty,
+            and hassle-free.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>Company</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>Get in Touch</h2>
+          <ul>
+            <li>+94-123-456-7890</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">© 2026 Tomato. All rights reserved.</p>
+    </div>
+  );
 };
 
 export default Footer;
