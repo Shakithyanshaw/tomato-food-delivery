295233e52770838e9f34a1181d2d6c4fa3d2d759
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-01-15 00:43:32 +0530

    Add PlaceOrder component styles and fix class name typo

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -25,7 +25,7 @@ const PlaceOrder = () => {
         </div>
         <input type="text" placeholder="Phone" />
       </div>
-      <div className="lace-order-right">
+      <div className="place-order-right">
         <div className="cart-total">
           <h2>Cart Totals</h2>
           <div>
