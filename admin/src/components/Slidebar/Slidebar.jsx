f94b9301b349563efbf1fbb69e36774094844901
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-01-22 15:11:17 +0530

    code admin slidebar

--- admin/src/components/Slidebar/Slidebar.jsx
+++ admin/src/components/Slidebar/Slidebar.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import './Slidebar.css';
+import { assets } from '../../assets/assets';
 
 const Slidebar = () => {
-  return <div>Slide Bar</div>;
+  return (
+    <div className="slidebar">
+      <div className="slidebar-options">
+        <div className="slidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Add Items</p>
+        </div>
+        <div className="slidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>List Items</p>
+        </div>
+        <div className="slidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>Order Items</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Slidebar;
